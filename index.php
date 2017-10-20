8a604c97031f58c0290edf48a68e7c0b5b6ebc98
m081779 <marcoprincipio@hotmail.com>
2017-10-20 03:24:57 -0400

    I think the code is good, but github won't allow POST

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
-		
 <?php
     $name = $_POST['name'];
     $email = $_POST['email'];
     $number = $_POST['number'];
     $message = $_POST['message'];
-    // $from = 'From: TangledDemo'; 
+    $from = 'From: TangledDemo'; 
     $to = 'marcoprincipio@hotmail.com'; 
     $subject = 'Hello';
     $human = $_POST['human'];
@@ -12,15 +11,14 @@
     $body = "From: $name\n E-Mail: $email\n Phone Number: $number\n Message:\n $message";
                 
     if ($_POST['submit'] && $human == '4') {                 
-        mail ($to, $subject, $body)  
+        if (mail ($to, $subject, $body, $from)) { 
         echo '<p>Your message has been sent!</p>';
-     
-        
+        } else { 
+            echo '<p>Something went wrong, go back and try again!</p>'; 
+        } 
     } 
-     else if ($_POST['submit'] && $human != '4') {
-    echo '<p>You answered the anti-spam question incorrectly!</p>';
-    } else {
-        echo '<p>Something went wrong, go back and try again!</p>'; 
+    else if ($_POST['submit'] && $human != '4') {
+        echo '<p>You are CLEARLY not a human.  Begone robot!</p>';
     }
 ?>
-				
+					
